ebe327cca5ea9dd10facc0908780c000c17b32a8
shaunnope <seanyap123@gmail.com>
2020-11-29 20:11:58 +0800

    Added rotate method for Board

--- board.py
+++ board.py
@@ -31,9 +31,12 @@ class Cube:
         self.root = root
 
         # display parameters
-        self.corner = (200,80)
-        self.side = 60
         self.font = ('Bauhaus 93 Regular',20)
+        self.padding = 0.1
+        self.corner = (1000,380)
+        self.side = 15
+        self.play_corner = (380,120)
+        self.play_side = 100
 
         # score tracking
         self.turn_num = 0
@@ -42,9 +45,24 @@ class Cube:
 
         self.boards = []
         self.init_cube(num_boards)
+        self.playfield = Board(self, self.root, self.play_corner, self.play_side)
+
+        self.init_controls()
         
         self.display_cube()
-    
+        self.debug = tk.Button(self.root, text = 'Debug', command=self.debug_func)
+        self.debug.place(x= 20, y= 20, width=50, height=20)
+
+    def debug_func(self):
+        self.playfield.rotate(1)
+
+    def init_scoreboard(self):
+        self.label=tk.Label(text=f"O wins: {self.wins[1]}\nX wins: {self.wins[2]}",bg='#00ffff',
+                            font=self.font)
+        self.label.place(x=800,y=80)
+    def update_scoreboard(self):
+        self.label['text'] = f"O wins: {self.wins[1]}\nX wins: {self.wins[2]}"
+  
     def init_cube(self, num_boards):
         if num_boards == 6:
             # init cube projection of boards
@@ -52,22 +70,46 @@ class Cube:
             w = self.side*3
             corners = [(x+w,y), (x,y+w), (x+w,y+w), (x+2*w,y+w), (x+3*w,y+w), (x+w,y+2*w)]
             for i in range(num_boards):
-                self.boards.append(Board(self, self.root, corners[i]))
+                self.boards.append(Board(self, self.root, corners[i], self.side))
         else:
             # if not default number of boards, display boards as a row
             for i in range(num_boards):
                 corner = (self.corner[0] + i*self.side*3, self.corner[1])
-                self.boards.append(Board(self, self.root, corner))
+                self.boards.append(Board(self, self.root, corner, self.side))
 
-    def init_scoreboard(self):
-        self.label=tk.Label(text=f"O wins: {self.wins[1]}\nX wins: {self.wins[2]}",bg='#00ffff',
-                            font=self.font)
-        self.label.place(x=800,y=80)
+    def init_controls(self):
+        # init list of control buttons [up(3), down(6), left(3), right(6)]
+        self.controls = [[None,None,None],[None,None,None,None,None,None],[None,None,None],[None,None,None,None,None,None]]
+        p = int(self.padding*self.play_side)
+        x,y = self.play_corner
+        w,h = self.play_side,self.play_side/3
+        corners = [(x,y-h-p),(x+w,y-h-p),(x+2*w,y-h-p),
+                    (x,y+3*w+p),(x+w,y+3*w+p),(x+2*w,y+3*w+p),
+                    (x,y+3*w+h+2*p),(x+w,y+3*w+h+2*p),(x+2*w,y+3*w+h+2*p),
+                    (x-h-p,y),(x-h-p,y+w),(x-h-p,y+2*w),
+                    (x+3*w+p,y),(x+3*w+p,y+w),(x+3*w+p,y+2*w),(x+3*w+h+2*p,y),(x+3*w+h+2*p,y+w),(x+3*w+h+2*p,y+2*w)]
+        dim = [(w,h),(h,w)]
+        symbs = ['^','v','<','>']
+        index = 0
+        for i in range(4):
+            num = 3*(i%2+1)
+            for j in range(num):
+                self.controls[i][j] = tk.Button(self.root, text = symbs[i])
+                self.controls[i][j].place(x= corners[index][0], y= corners[index][1], width=dim[i>1][0], height=dim[i>1][1])
+                index +=1
 
-    def update_scoreboard(self):
-        self.label['text'] = f"O wins: {self.wins[1]}\nX wins: {self.wins[2]}"
+    # recursive function to rotate cube down
+    def rotate_down(self, times = 1):
+        order = [0,2,5,4]
+        order = order[times-4:times-1]
+        order = [order[0], 1, order[1], 3, order[3], order[2]]
+        old_boards = deepcopy(self.boards)
+        self.boards = [old_boards[i] for i in order]
+        pass
 
+    # displays all the Board instances
     def display_cube(self):
+        self.playfield.display_board()
         for board in self.boards:
             board.display_board()
 
@@ -109,15 +151,18 @@ class Cube:
 
 class Board(Cube):
     # init empty board and board score
-    def __init__(self, parent=None, root=None, corner = (380,80)):
+    def __init__(self, parent=None, root=None, corner = (380,80), side = 15):
         if parent is None:
             super().__init__(root)
         self.parent = parent
         self.root = root
+
+        # get parameters
+        self.side = side
+        self.corner = corner
+
         self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.wins = {1: 0, 2: 0} # 1: circle, 2: cross
-        self.side = 60
-        self.corner = corner
 
         self.buttons = [[None, None, None], [None, None, None], [None, None, None]]
 
@@ -127,8 +172,31 @@ class Board(Cube):
         for i in range(3):
             for j in range(3):
                 symb = 'O'*(self.board[i][j] == 1) + 'X'*(self.board[i][j] == 2)
-                self.buttons[i][j] = tk.Button(self.root, text = symb, command=partial(self.update_board,(i,j)))
+                self.buttons[i][j] = tk.Button(self.root, text = symb, command=partial(self.make_move,(i,j)))
                 self.buttons[i][j].place(x= x + i * self.side, y= y + j * self.side, width=self.side, height=self.side)
+    def update_board(self):
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j]['text'] = 'O'*(self.board[i][j] == 1) + 'X'*(self.board[i][j] == 2)
+
+    # recusive function to rotate orientation of a face clockwise a given number of times
+    def rotate(self, times = 2):
+        times %= 4
+        if times == 0:
+            return
+
+        old_board = deepcopy(self.board)
+        self.board[0][0] = old_board[2][0]
+        self.board[0][1] = old_board[1][0]
+        self.board[0][2] = old_board[0][0]
+        self.board[1][0] = old_board[2][1]
+        self.board[1][2] = old_board[0][1]
+        self.board[2][0] = old_board[2][2]
+        self.board[2][1] = old_board[1][2]
+        self.board[2][2] = old_board[0][2]
+        self.update_board()
+        self.rotate(times-1)
+
 
     # returns True if coords provided is empty cell
     # False otherwise
@@ -152,7 +220,7 @@ class Board(Cube):
     # attempt to update board with given move
     # if valid move, update board and inc turn_num
     # if invalid, display error window
-    def update_board(self, coords = (0,0)):
+    def make_move(self, coords = (0,0)):
         # get current turn number from parent
         turn_num = self.parent.turn_num
         i,j = coords
